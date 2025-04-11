37e157b23572758070b102608d8cfc784eac2470
Diego M <Diego M@DESKTOP-JRHMECS>
2025-04-11 01:18:14 -0700

    Se modifico la tabla y termino la tabla students

--- src/main/java/mx/itson/classroom/ui/StudentList.java
+++ src/main/java/mx/itson/classroom/ui/StudentList.java
@@ -115,7 +115,7 @@ public class StudentList extends javax.swing.JFrame {
     }//GEN-LAST:event_formWindowOpened
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-         Student inicio = new Student();
+         StudentForm inicio = new StudentForm();
         inicio.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 private void cargarTable(){
